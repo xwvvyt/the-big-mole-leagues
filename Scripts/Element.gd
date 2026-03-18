dce0cffee15ec87f078874297c48ebfe4fe643ac
manuelolea <olea.fregoso@gmail.com>
2026-03-18 17:06:52 -0400

    Update Element.gd

--- Scripts/Element.gd
+++ Scripts/Element.gd
@@ -1,40 +1,53 @@
 extends Node
 
 var ELEMENTS: Dictionary = {
+	"H": {
+		"type": "Hydrogene",
+		"symbole": "H",
+		"max_liaisons": 1,
+		"liaisons_actuelles": 0,
+		"numero_atomique" :1
+	},
 	"C": {
 		"type": "Carbone",
 		"symbole": "C",
 		"max_liaisons": 4,
 		"liaisons_actuelles": 0
 	},
-	"H": {
-		"type": "Hydrogene",
-		"symbole": "H",
-		"max_liaisons": 1,
-		"liaisons_actuelles": 0
+	"N": {
+		"type": "Azote",
+		"symbole": "N",
+		"max_liaisons": 4,
+		"liaisons_actuelles": 0,
+		"numero_atomique" :7
 	},
 	"O": {
 		"type": "Oxygene",
 		"symbole": "O",
-		"max_liaisons": 2,
-		"liaisons_actuelles": 0
-	},
-	"N": {
-		"type": "Azote",
-		"symbole": "N",
 		"max_liaisons": 3,
-		"liaisons_actuelles": 0
+		"liaisons_actuelles": 0,
+		"numero_atomique" :8
 	},
+	"P" :{
+		"type": "Phosphore",
+		"symbole": "P",
+		"max_liaisons": 5,
+		"liaisons_actuelles": 0,
+		"numero_atomique": 15
+	},
+	
 	"S": {
 		"type": "Soufre",
 		"symbole": "S",
-		"max_liaisons": 2,
-		"liaisons_actuelles": 0
+		"max_liaisons": 6,
+		"liaisons_actuelles": 0,
+		"numero_atomique": 16
 	},
-	"Benzene": {
-		"type": "Benzene",
-		"symbole": "C6H6",
-		"max_liaisons": 3,
-		"liaisons_actuelles": 0
-	}
+	"Cl": {
+		"type": "Chlore",
+		"symbole": "Cl",
+		"max_liaisons": 1,
+		"liaisons_actuelles": 0,
+		"numero_atomique": 17
+	},
 }
